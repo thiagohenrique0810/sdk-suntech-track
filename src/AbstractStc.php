8fc3bd5823c2368f25211bc36d04dcb79476f06d
Thiago Henrique <thiagohenrique0810@hotmail.com>
2022-01-29 17:22:29 -0300

    restruturando classes para melhorar desempenho

--- src/AbstractStc.php
+++ src/AbstractStc.php
@@ -26,17 +26,62 @@ class AbstractStc {
 
 	public function listbrand($data)
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicles/listbrand');
+    	}
 	}
 
 	public function addbrand($data) 
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicle/addbrand');
+    	}
 	}
 
 	public function removebrand($data)
 	{
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicle/removebrand');
+    	}
+	}
 
+	public function listModel($data)
+	{
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicles/listmodel');
+    	}
+	}
+
+	public function addModel($data) 
+	{
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicle/addmodel');
+    	}
+	}
+
+	public function removeModel($data)
+	{
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicle/removemodel');
+    	}
 	}
 
 	public function addClient($data) 
@@ -44,9 +89,7 @@ class AbstractStc {
 		if(!empty($data)) {
 			//die($this->key);
 	        return sendRequest(array_merge($data, [
-
 	        	'key' => $this->getKey()
-
 	        ]), $this->getUri() . 'client/add');
     	}
 	}
@@ -56,100 +99,199 @@ class AbstractStc {
 		if(!empty($data)) {
 			//die($this->key);
 	        return sendRequest(array_merge($data, [
-
 	        	'key' => $this->getKey()
-
 	        ]), $this->getUri() . 'client/update');
     	}
 	}
 
 	public function removeClient($data)
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	         return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'client/remove');
+    	}
 	}
 
 	public function listAllClient($data)
 	{
 		if(!empty($data)) {
 			//die($this->key);
-	        return sendRequest([
-
-	        	'key' => $this->getKey(),
-	        	'page' => $data['page'],
-	        	'cpfcnpj' => $data['cpfcnpj']
-
-	        ], $this->getUri() . 'client/list');
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'client/list');
     	}
 	}
 
 	public function addContact($data) 
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'client/addcontact');
+    	}
 	}
 
 	public function updateContact($data) 
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'client/updatecontact');
+    	}
 	}
 
 	public function removeContact($data)
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'client/removecontact');
+    	}
 	}
 
 	public function addDevice($data) 
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'device/add');
+    	}
 	}
 
 	public function listAllDevice($data)
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'device/list');
+    	}
 	}
 
 	public function listmanagerDevice($data)
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'device/listmanager');
+    	}
 	}
 
 	public function associateDevice($data)
 	{
-
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'device/associate');
+    	}
 	}
 
 	public function updateLogin($data) 
 	{
-		
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'client/updateLgwFunc');
+    	}
 	}
 
 	public function changeonwerVehicle($data) 
     {
-
+    	if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'client/changeonwer');
+    	}
     }
 
     public function addVehicle($data) 
     {
-
+    	if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicle/add');
+    	}
     }
 
     public function updateVehicle($data) 
     {
-
+    	if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicle/update');
+    	}
     }
 
     public function removeVehicle($data)
     {
-
+    	if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicle/remove');
+    	}
     }
 
     public function listAllVehicle($data)
     {
-        
+        if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicle/list');
+    	}
     }
 
-    public function listtype($data)
+    public function listVehicleType($data)
 	{
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'vehicle/listtype');
+    	}
+	}
 
+	public function listCities($data)
+	{
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'client/getcities');
+    	}
+	}
+
+	public function vehicleHistoryPositions($data)
+	{
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'admin/getVehiclePositionsByLimit500');
+    	}
+	}
+
+	public function listManufactures($data)
+	{
+		if(!empty($data)) {
+			//die($this->key);
+	        return sendRequest(array_merge($data, [
+	        	'key' => $this->getKey()
+	        ]), $this->getUri() . 'manufacture/list');
+    	}
 	}
 
     /**
---
+++ tests/addContact.php
@@ -0,0 +1,24 @@
+<?php
+
+
+//REALIZANDO TESTES
+
+
+//incluindo lib
+include_once '../SuntechAPI.php';
+
+
+$options = [
+	'key' => '17bfae24cd65753c0a577e8f65a405cf',
+	'sandbox' => true,
+];
+
+$suntech = new SuntechAPI($options);
+
+$data = [
+	'id' => '4',
+];
+
+print_r($suntech->removeClient($data));
+
+
